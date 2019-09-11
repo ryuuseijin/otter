f09bbb16f7880a8909b05677cb7300462c6690a7
ryuuseijin <ryuuseijin@example.com>
2019-09-11 22:28:22 +1000

    refactor: introduce invert/prepare to associate deleted nodes with ops

--- src/otter/invert.clj
+++ src/otter/invert.clj
@@ -2,61 +2,113 @@
   (:require [otter.operations :as op]
             [otter.utils :refer :all]))
 
-(defmulti invert-in-seq (fn [op nodes] (:type op)))
+(defmulti prepare-op-in-seq (fn [op nodes]
+                           (:type op)))
 
-(defn invert-op [op node]
-  (let [[op' _] (invert-in-seq op [node])]
+(defn prepare [op tree]
+  (let [[op' _] (prepare-op-in-seq op [tree])]
     op'))
 
-(defn invert-seq [ops nodes]
-  (let [[r _] (reduce (fn [[r nodes] op]
-                        (let [[op' nodes] (invert-in-seq op nodes)]
-                          [(conj r op') nodes]))
-                      [[] nodes]
-                      ops)]
-    r))
+(defn prepare-seq [ops nodes]
+  (first
+   (reduce (fn [[r nodes] op]
+             (let [[op' nodes'] (prepare-op-in-seq op nodes)]
+               [(conj r op') nodes']))
+           [[] nodes]
+           ops)))
 
-(defn invert-map [op-map node-map]
+(defn prepare-map [op-map node-map]
   (into {}
         (map (fn [[k op]]
-               [k (invert-op op (get node-map k))]))
+               [k (prepare-op op (get node-map k))]))
         op-map))
 
-(defn invert-subtree [subtree tree]
+(defn prepare-subtree [subtree node]
   (cond
     (and (sequential? subtree)
-         (sequential? tree))
-    (invert-seq subtree tree)
-    
-    (and (map? subtree)
-         (map? tree))
-    (invert-map subtree tree)
-    
-    :else (panic "unable to invert retain-subtree with mismatched subtree type")))
+         (sequential? node))
+    (prepare-seq subtree node)
 
-(defmethod invert-in-seq :insert-values [op nodes]
-  [(op/delete-range (count (:values op)))
+    (and (map? subtree)
+         (map? node))
+    (prepare-map subtree node)
+
+    :else (panic "unable to prepare mismatched subtree types")))
+
+(defmethod prepare-op-in-seq :insert-values [op nodes]
+  [op
    nodes])
 
-(defmethod invert-in-seq :retain-range [op nodes]
+(defmethod prepare-op-in-seq :retain-range [op nodes]
   [op
    (drop (:retain-length op) nodes)])
 
-(defmethod invert-in-seq :retain-subtree [op nodes]
-  [(assoc op :subtree (invert-subtree (:subtree op) (first nodes)))
+(defmethod prepare-op-in-seq :retain-subtree [op nodes]
+  [(update op :subtree prepare-subtree (first nodes))
    (next nodes)])
 
-(defmethod invert-in-seq :delete-range [op nodes]
-  [(op/insert-values (take (:delete-length op) nodes))
+(defmethod prepare-op-in-seq :delete-range [op nodes]
+  [(vary-meta op assoc ::deleted-values (take (:delete-length op) nodes))
    (drop (:delete-length op) nodes)])
 
-(defmethod invert-in-seq :replace-value [op nodes]
-  [(op/replace-value (first nodes))
+(defmethod prepare-op-in-seq :replace-value [op nodes]
+  [(vary-meta op assoc ::replaced-value (first nodes))
    (next nodes)])
 
-(defmethod invert-in-seq :mark [op nodes]
-  [op
-   nodes])
+(defmethod prepare-op-in-seq :mark [op nodes]
+  [op nodes])
 
-(defn invert [delta tree]
-  (invert-op (:root-op delta) tree))
+(defmulti invert-op :type)
+
+(defn invert-seq [ops]
+  (into [] (map invert-op ops)))
+
+(defn invert-map [op-map]
+  (into {}
+        (map (fn [[k op]]
+               [k (invert-op op)]))
+        op-map))
+
+(defn invert-subtree [subtree]
+  (cond
+    (sequential? subtree)
+    (invert-seq subtree)
+    
+    (map? subtree)
+    (invert-map subtree)
+    
+    :else (panic)))
+
+(defmethod invert-op :insert-values [op]
+  (-> (op/delete-range (count (:values op)))
+      (vary-meta assoc ::deleted-values (:values op))))
+
+(defmethod invert-op :retain-range [op]
+  op)
+
+(defmethod invert-op :retain-subtree [op]
+  (update op :subtree invert-subtree))
+
+(defn panic-unprepared []
+  (panic "unable to invert unprepared delta; use (invert delta tree) or (invert (prepare delta tree))"))
+
+(defmethod invert-op :delete-range [op]
+  (if-let [[_ values] (-> op meta (find ::deleted-values))]
+    (op/insert-values values)
+    (panic-unprepared)))
+
+(defmethod invert-op :replace-value [op]
+  (if-let [[_ value] (-> op meta (find ::replaced-value))]
+    (-> (op/replace-value value)
+        (vary-meta assoc ::replaced-value (:value op)))
+    (panic-unprepared)))
+
+(defmethod invert-op :mark [op]
+  op)
+
+(defn invert
+  ([delta] (invert-op delta))
+  ([delta tree]
+   (-> delta
+       (prepare tree)
+       (invert))))
--- test/otter/invert_test.clj
+++ test/otter/invert_test.clj
@@ -1,65 +1,105 @@
 (ns otter.invert-test
   (:require [clojure.test :refer :all]
             [otter.operations :as op]
-            [otter.invert :refer [invert-seq invert-map]]))
+            [otter.invert :refer [invert prepare]]))
 
 (deftest insert-values
   (testing "invert insert-values"
     (testing "in sequences"
-      (is (= [(op/delete-range 3)]
-             (invert-seq [(op/insert-values [1 2 3])]
-                         [1 2 3 4]))))
+      (is (= (op/retain-subtree
+              [(op/delete-range 3)])
+             (invert (op/retain-subtree
+                      [(op/insert-values [1 2 3])])
+                     [1 2 3 4]))))
     (testing "in maps"
-      (is (= {:key (op/delete-range 1)}
-             (invert-map {:key (op/insert-values [:val])}
-                         {:key :val}))))))
+      (is (= (op/retain-subtree
+              {:key (op/delete-range 1)})
+             (invert (op/retain-subtree
+                      {:key (op/insert-values [:val])})
+                     {:key :val}))))))
 
 (deftest retain-range
   (testing "invert retain-range"
     (testing "in sequences"
-      (is (= [(op/retain-range 1)]
-             (invert-seq [(op/retain-range 1)]
-                         [:val]))))
+      (is (= (op/retain-subtree
+              [(op/retain-range 1)])
+             (invert (op/retain-subtree
+                      [(op/retain-range 1)])
+                     [:val]))))
     (testing "in maps"
-      (is (= {:key (op/retain-range 1)}
-             (invert-map {:key (op/retain-range 1)}
-                         {:key :val}))))))
+      (is (= (op/retain-subtree
+              {:key (op/retain-range 1)})
+             (invert (op/retain-subtree
+                      {:key (op/retain-range 1)})
+                     {:key :val}))))))
 
 (deftest retain-subtree
   (testing "invert retain-subtree"
     (testing "in sequences"
-      (is (= [(op/retain-subtree [(op/insert-values [1 2 3])])]
-             (invert-seq [(op/retain-subtree [(op/delete-range 3)])]
-                         [[1 2 3]]))))
+      (is (= (op/retain-subtree
+              [(op/retain-subtree [(op/insert-values [1 2 3])])])
+             (invert (op/retain-subtree
+                      [(op/retain-subtree [(op/delete-range 3)])])
+                     [[1 2 3]]))))
     (testing "in maps"
-      (is (= {:key (op/retain-subtree {:key (op/insert-values [:val])})}
-             (invert-map {:key (op/retain-subtree {:key (op/delete-range 1)})}
-                         {:key {:key :val}}))))))
+      (is (= (op/retain-subtree
+              {:key (op/retain-subtree {:key (op/insert-values [:val])})})
+             (invert (op/retain-subtree
+                      {:key (op/retain-subtree {:key (op/delete-range 1)})})
+                     {:key {:key :val}}))))))
 
 (deftest delete-range
   (testing "invert delete-range"
     (testing "in sequences"
-      (is (= [(op/insert-values [1 2 3])]
-             (invert-seq [(op/delete-range 3)]
-                         [1 2 3 4]))))
+      (is (= (op/retain-subtree
+              [(op/insert-values [1 2 3])])
+             (invert (op/retain-subtree
+                      [(op/delete-range 3)])
+                     [1 2 3 4]))))
     (testing "in maps"
-      (is (= {:key (op/insert-values [:val])}
-             (invert-map {:key (op/delete-range 1)}
-                         {:key :val}))))))
+      (is (= (op/retain-subtree
+              {:key (op/insert-values [:val])})
+             (invert (op/retain-subtree
+                      {:key (op/delete-range 1)})
+                     {:key :val}))))))
+
+(deftest invert-previously-inverted
+  (testing "a previously inverted delete-range does not need to be prepared"
+    (is (= (op/retain-subtree
+            [(op/insert-values [1 2 3])])
+           (-> (prepare (op/retain-subtree [(op/delete-range 3)])
+                        [1 2 3 4])
+               (invert) ;; insert-values
+               (invert) ;; delete-range
+               (invert) ;; insert-values
+               ))))
+  (testing "a previously inverted replace-value does not need to be prepared"
+    (is (= (op/retain-subtree
+            [(op/replace-value :val1)])
+           (-> (prepare (op/retain-subtree [(op/replace-value :val2)])
+                        [:val1])
+               (invert) ;; replace-value :val1
+               (invert) ;; replace-value :val2
+               (invert) ;; replace-value :val1
+               )))))
 
 (deftest replace-value
   (testing "invert replace-value"
     (testing "in sequences"
-      (is (= [(op/replace-value :val1)]
-             (invert-seq [(op/replace-value :val2)]
-                         [:val1]))))
+      (is (= (op/retain-subtree
+              [(op/replace-value :val1)])
+             (invert (op/retain-subtree
+                      [(op/replace-value :val2)])
+                     [:val1]))))
     (testing "in maps"
-      (is (= {:key (op/replace-value :val1)}
-             (invert-map {:key (op/replace-value :val2)}
-                         {:key :val1}))))))
+      (is (= (op/retain-subtree
+              {:key (op/replace-value :val1)})
+             (invert (op/retain-subtree
+                      {:key (op/replace-value :val2)})
+                     {:key :val1}))))))
 
 (deftest mark
   (testing "invert mark"
-    (is (= [(op/mark "id")]
-           (invert-seq [(op/mark "id")]
-                       [])))))
+    (is (= (op/retain-subtree [(op/mark "id")])
+           (invert (op/retain-subtree [(op/mark "id")])
+                   [])))))
